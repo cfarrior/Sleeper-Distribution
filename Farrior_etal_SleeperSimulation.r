846f894ec0113772094097aa1fdc48c0dd9ffa1a
Caroline Farrior <cfarrior@gmail.com>
2016-01-14 11:01:28 -0500

    Update Farrior_etal_SleeperSimulation.r

--- Farrior_etal_SleeperSimulation.r
+++ Farrior_etal_SleeperSimulation.r
@@ -1,6 +1,5 @@
-#Simulation model from the paper: 
-#Farrior, Bohlman, Hubbell and Pacala. "Dominance of the suppressed: Power law size strucutre in tropical forests"
-#Last updated 15 Nov 2015   cfarrior@gmail.com
+#Simulation model of "Dominance of the Suppressed", Farrior et al. In Review
+#Last updated 15 July 2015   cfarrior@gmail.com
 
 #To run, create a folder: "c:/usr/Farrior_etal_SleeperDist/"
 #enter the following in R without the #'s: 
@@ -8,13 +7,18 @@
 #source("Farrior_etal_SleeperSimulation.r")
 #runV = c(filestem="PaperParameters",PA=1000,deltaT=1,Fnot=0.02,dnot=.02,G.c=6.05,G.u=.534,mu.c=0.0194/2,mu.u=0.0347-0.0194/2,mu=0.0194/2,cutT=400,landscape_m2=125000,censuscutT = 5)
 #main_cohorts(runV=runV,tag=1,newplot=TRUE,plotting=TRUE)
+#When plotting = TRUE, the simulation runs much more slowly but you can watch the patch develop and crash. 
+#The best fit BCI power law (Fig. 1) is added for comparison. 
+#*'s indicate empty bins and blue line is a smoothing of the data to incorporate those empty bins. 
+#To finish, the size distribution for all recorded snapshots of the simulation is plotted in the same way. 
 
+phi = 0.03615016; theta = 1.2819275
 
 ######################################
-main_cohorts = function(runV=c(filestem="default",PA=1000,deltaT=1,Fnot=0.05,dnot=0.02,G.c=6.05,G.u=0.534,mu.c=0.0097,mu.u=0.025,mu=0.0097,cutT=400,landscape_m2=125000,censuscutT=5),tag=1,newplot=FALSE,plotting=TRUE){
+main_cohorts = function(runV=c(filestem="default",PA=1000,deltaT=1,Fnot=0.05,dnot=0.02,G.c=6.05,G.u=0.534,mu.c=0.0097,mu.u=0.025,mu=0.0097,cutT=400,landscape_m2=125000),tag=1,newplot=FALSE,plotting=TRUE){
 #runs the cohorts model
-#filestem: the name to call the output files
-#tag: an identifier for the specific run
+#runV: contains all of the inputs for the simulation, including:
+	#filestem: the name to call the output files
 #PA: size in m2 of the plot
 #deltaT: timestep in years
 #Fnot: individuals produced per m2 of sun-lit crown area at dnot (reproduction to seedling)
@@ -23,16 +27,16 @@ main_cohorts = function(runV=c(filestem="default",PA=1000,deltaT=1,Fnot=0.05,dno
 #G.u: the diameter growth, in mm/year of individuals in the understory
 #mu.c: the mortality rate in years^-1 of individuals in the sun
 #mu.u: the mortality rate in years^-2 of individuals in the understory
-#maxT: the runtime in years of the run
+#mu: the stand clearing disturbance rate
 #cutT: the time between recording snapshots of the forest - these used to build a landscape of independent forests
-#newplot: whether to create a new plotting window
-#plotting: whether to plot the size distribution during the simulation, slows the run speed considerably
-#maxT: the run time in years
-#wipeout: logical, TRUE if the stand is resent to starting conditions at uniform yearly probability (run at the timestep level) of mu
+#landscape_m2: the desired size of the final assembled landscape (landscape_m2 = PA*[total run time]/cutT)
+#tag: an identifier for the specific run
+#newplot: logical, whether to create a new plotting window
+#plotting: logical, whether to plot the size distribution during the simulation, when TRUE slows the run speed considerably
 ######################################
 	filestem = runV[names(runV)=="filestem"][[1]]
 
-	if(tag==1) write.table(cbind(date(),t(runV)),paste(mainfolder,"main_cohorts_LOG.txt",sep=""),append=TRUE,sep="\t",col.names=filestem=="start",row.names=FALSE)
+	if(tag==1) write.table(cbind(date(),t(runV)),paste("main_cohorts_LOG.txt",sep=""),append=TRUE,sep="\t",col.names=filestem=="start",row.names=FALSE)
 	
 	PA = as.numeric(runV[names(runV)=="PA"][[1]])
 	deltaT = as.numeric(runV[names(runV)=="deltaT"][[1]])
@@ -42,11 +46,9 @@ main_cohorts = function(runV=c(filestem="default",PA=1000,deltaT=1,Fnot=0.05,dno
 	G.u = as.numeric(runV[names(runV)=="G.u"][[1]])
 	mu.c = as.numeric(runV[names(runV)=="mu.c"][[1]])
 	mu.u = as.numeric(runV[names(runV)=="mu.u"][[1]])
-	mu.u_exp = as.numeric(runV[names(runV)=="mu.u_exp"][[1]])
 	mu = as.numeric(runV[names(runV)=="mu"][[1]])
 	cutT = as.numeric(runV[names(runV)=="cutT"][[1]])
 	landscape_m2 = as.numeric(runV[names(runV)=="landscape_m2"][[1]])
-	censuscutT = as.numeric(runV[names(runV)=="censuscutT"][[1]])
 	
 	maxT = landscape_m2/PA*cutT
 
@@ -70,36 +72,36 @@ main_cohorts = function(runV=c(filestem="default",PA=1000,deltaT=1,Fnot=0.05,dno
 		}
 		data = data[data[,2]>0,,drop=FALSE]
 		#Step 2: Growth
-		data[data[,3]==1,1] = data[data[,3]==1,1]+G.c*deltaT
-		data[data[,3]==2,1] = data[data[,3]==2,1]+G.u*deltaT
+		data[data[,3]==1,1] = data[data[,3]==1,1]+(G.c+rnorm(1,0,.3))*deltaT
+		data[data[,3]==2,1] = data[data[,3]==2,1]+(G.u+rnorm(1,0,.01))*deltaT
 		data = data[data[,1]>0,,drop=FALSE]
 
 		#Step 3: Reproduce
 		babies = round(PA*Fnot*deltaT) #dispersal from far away
 		if(babies>0){
-			babyMatrix = matrix(1,nrow=1,ncol=4)
+			babyMatrix = matrix(1,nrow=1,ncol=3)
 			CA = sum(phi*data[,1]^theta*data[,2])
 			babyMatrix[,1]=dnot; babyMatrix[,2]=babies
 			if(CA>PA) babyMatrix[,3]=2
-			babyMatrix[,4]=0
 			data = rbind(data,babyMatrix)
 		}
 			
 		#Step 4: Assign crown class
 		CA = sum(phi*data[,1]^theta*data[,2])
-		if(CA<=PA){ data[,3]=1; data[,4]=0}
+		if(CA<=PA) data[,3]=1
 		if(CA>PA) data = CCassign(data,PA,deltaT)
 		
 		#Step 5: Record and plot
 		if(floor(t/cutT)==t/cutT){
-			if(dim(data)[1]>0){
-				dstar = find_dstar(get_dV(data),PA)
-				CAi = sum(data[data[,1]>10,1]^theta*phi*data[data[,1]>10,2])/PA
-				write.table(cbind(data,t,t_age,dstar,CAi),paste(filestem,tag,".txt",sep=""),sep="\t",col.names=t==cutT,row.names=FALSE,append=t!=cutT)
-			}
+			if(dim(data)[1]>0) write.table(data,paste(filestem,tag,".txt",sep=""),sep="\t",col.names=t==cutT,row.names=FALSE,append=t!=cutT)
 		}
 		if(plotting) if(max(data[,1])>10) bdata = SizeDistPlot(data,sizem2=PA,xmax=5000,ymin=1e-4)
 	}
+	
+	tdata = read.table(paste(runV[[1]],tag,".txt",sep=""),sep="\t",header=TRUE)
+	tbdata = SizeDistPlot(as.matrix(tdata),sizem2=as.numeric(runV[names(runV)=="landscape_m2"][[1]]),main=paste(runV[[1]],tag))
+
+	
 }# end main_cohorts
 
 ######################################
@@ -133,10 +135,10 @@ CCassign = function(data,PA,deltaT){
 
 	
 ######################################
-SizeDistPlot = function(data,logby=.14,win=3,sizem2=NaN,main="",census=5,ploton=TRUE,newplot=TRUE,col=1,xaxt="s",yaxt="s",ymin=1e-4,xmax=3500,binV=NaN,justbdata=FALSE,powerfit=allfit){
-#function used in the main_cohorts, main_individuals_forcensus simulations to plot the size distribution during the simulation run
+SizeDistPlot = function(data,logby=.14,win=3,sizem2=NaN,main="",census=5,ploton=TRUE,newplot=TRUE,col=1,xaxt="s",yaxt="s",ymin=1e-4,xmax=3500,binV=NaN,justbdata=FALSE,powerfit=list(alpha=2.1283,xmin=22,C=19.6211)){
+#Minimal plotting function used in the main_cohorts simulations to plot the size distribution during the simulation run
 #this function slows down the speed of the simulation quite a bit.
-#data: either a vector of diameters of individuals in the community OR a matrix with the first column the diameter and the second column the number of individuals (This flexibility is new and needs testing. Before data could only be the matrix. This might make some old function unnecessary. 150408 !?)
+#data: either a vector of diameters of individuals in the community OR a matrix with the first column the diameter and the second column the number of individuals
 #logby: the bin width in logspace for the diameter bins
 #win: the number of bins on either side of the target bin to use to smooth over (win=3, makes a window size of 7 bins actually)
 #sizem2: the size of the sampled area in m2
